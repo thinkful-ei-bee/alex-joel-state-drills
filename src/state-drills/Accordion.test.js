e65f5782d5e7f7ed5c0c3c71dfb8c2b008f63a3c
Alex Varghese <alex.varghese250@gmail.com>
2019-03-20 14:12:56 -0400

    finished testing

--- src/state-drills/Accordion.test.js
+++ src/state-drills/Accordion.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Accordion from './Accordion';
 import ReactDOM from 'react-dom';
 import {sections} from './AccordionStore';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 
 describe('Accordion Test', () =>{
@@ -12,8 +14,25 @@ describe('Accordion Test', () =>{
       });
 
     it('Accordion renders empty without prop',() =>{
-        const div = document.createElement('div');
-        ReactDOM.render(<Accordion />, div);
-        ReactDOM.unmountComponentAtNode(div);
+        const wrapper = shallow(<Accordion />)
+        expect(toJson(wrapper)).toMatchSnapshot()
     })
+
+    it('renders no active sections', () => {
+        const wrapper = shallow(<Accordion sections={sections} />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+      })
+
+      it('renders a clicked section', () => {
+        const wrapper = shallow(<Accordion sections={sections} />)
+        wrapper.find('button').at(1).simulate('click')
+        expect(toJson(wrapper)).toMatchSnapshot()
+      })
+
+      it('renders only last section when multiple have been clicked', () => {
+        const wrapper = shallow(<Accordion sections={sections} />)
+        wrapper.find('button').at(1).simulate('click')
+        wrapper.find('button').at(2).simulate('click')
+        expect(toJson(wrapper)).toMatchSnapshot()
+      })
 })
