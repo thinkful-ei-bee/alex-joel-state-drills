d6d153ac9ce6731800316d4978459b53712c0503
joelcorey <joelcorey@fastmail.com>
2019-03-20 07:40:36 -0700

    add state update for pulling trigger

--- src/state-drills/RouletteGun.js
+++ src/state-drills/RouletteGun.js
@@ -3,17 +3,20 @@ import React from 'react';
 class RouletteGun extends React.Component {
   static defaultProps = {
     bulletInChamber: 8,
+    message: "you're safe!",
   };
   constructor(props) {
     super(props)
-    this.state = { 
+    this.state = {
+      message: null,
       chamber: null,
       spinningTheChamber: false
     };
   }
   componentDidMount() {
     this.setState({
-      chamber: this.props.bulletInChamber
+      chamber: this.props.bulletInChamber,
+      message: this.props.message
     })
   }
   componentWillUnmount() {
@@ -23,25 +26,41 @@ class RouletteGun extends React.Component {
     // const newCount = this.state.count + 1;
     console.log('spinning the chamber');
     this.setState({
+      message: "spinning the chamber and pulling the trigger! ...",
       spinningTheChamber: true
     })
     
     setTimeout(() => {
       let random = Math.ceil(Math.random() * 8);
+      
       console.log(`You spun the chamber to: ${random} state.chamber: ${this.state.chamber}`)
       
       if(random === this.state.chamber) {
-        console.log('BANG! you are dead');
+        this.setState({
+          message: "Bang!!!",
+          spinningTheChamber: false
+        })
       }
+
+      if(random !== this.state.chamber) {
+        this.setState({
+          message: "you're safe!",
+          spinningTheChamber: false
+        })
+      }
+
     }, 2000);
     
   }
   render() {
     return (
-      
-      <button onClick={() => this.handleButtonClick()}>
-        Pull the trigger
-      </button>
+      <div>
+        <hr />
+        <p>{this.state.message}</p>
+        <button onClick={() => this.handleButtonClick()}>
+          Pull the trigger
+        </button>
+      </div>
     )
   }
 }
